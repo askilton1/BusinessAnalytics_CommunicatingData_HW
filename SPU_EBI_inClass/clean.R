3897c725f05164a44ac3f0e2c79acb94ad273bc3
askilton1 <antonio.skilton@gmail.com>
2016-10-02 08:34:13 -0700

    clean() returns list
    accommodates LASSO models

--- SPU_EBI_inClass/clean.R
+++ SPU_EBI_inClass/clean.R
@@ -1,23 +1,29 @@
-clean <- function(df,plot=FALSE){
-suppressMessages(library(dplyr))
-suppressMessages(library(tidyr))
-suppressMessages(library(psych))
-source("my_aggr.R")
-
-##MISSING VARIABLES
-if(plot==TRUE) my_aggr(raw)#plot shows patterns of missing data in observations
-df<- raw %>%
-  select(-Inc_Cat) %>%
-  gather(key=response,value=rating,Electronics,Percrip_Med,Drn_Clnr,Dsps_Food) 
-if(plot==TRUE) my_aggr(df)
-
-##COLINEARITY
-if(plot==TRUE) pairs.panels(raw)#plot shows correlation between variables
-
-df <- df %>%
-  select(-Age_Cat)#colinear with Age variable
-
-if(plot==TRUE) pairs.panels(select(df,-response))#in second plot, response var has 0 correlation
-
-return(df)
+clean <- function(df,plot=FALSE,LASSO=TRUE,ALL=TRUE){
+  suppressMessages(library(dplyr))
+  suppressMessages(library(tidyr))
+  suppressMessages(library(psych))
+  source("my_aggr.R")
+  
+  ##MISSING VARIABLES
+  if(plot==TRUE) my_aggr(raw)#plot shows patterns of missing data in observations
+  df<- raw %>%
+    select(-Inc_Cat) %>%
+    gather(key=response,value=rating,Electronics,Percrip_Med,Drn_Clnr,Dsps_Food) 
+  if(plot==TRUE) my_aggr(df)
+  
+  ##COLINEARITY
+  if(plot==TRUE) pairs.panels(raw)#plot shows correlation between variables
+  data.ls <- list()
+  data.ls$df <- df %>%
+    select(-Age_Cat)#colinear with Age variable
+  
+  if(plot==TRUE) pairs.panels(select(df,-response))#in second plot, response var has 0 correlation
+  
+  if(LASSO==TRUE){
+    data.ls$electronicsLASSO <- model.matrix(~.,(select(filter(df,response=="Electronics"),-response)))
+    data.ls$percrip_medLASSO <- model.matrix(~.,(select(filter(df,response=="Percrip_Med"),-response)))
+    data.ls$drn_clnrLASSO <- model.matrix(~.,(select(filter(df,response=="Drn_Clnr"),-response)))
+    data.ls$dsps_foodLASSO <- model.matrix(~.,(select(filter(df,response=="Dsps_Food"),-response)))
+  }
+  return(data.ls)
 }
