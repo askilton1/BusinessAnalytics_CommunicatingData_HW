ce4e7bf3b1088b8f7801b6f937de9326b1275eb4
askilton1 <antonio.skilton@gmail.com>
2016-10-01 17:45:24 -0700

    condensed to for loop
    since each dataset was produced/cleaned in the same way, entire process
    was condensed to a for loop

--- SPU_EBI_inClass/clean.R
+++ SPU_EBI_inClass/clean.R
@@ -3,67 +3,19 @@ source("my_aggr.R")
 
 data <- tbl_df(read.csv("2_EBI_Data.csv"))
 
-#data cleaned to maxize size of pure data set
-#1. select one of four measures
-#2. filter out observations that have missing value for selected measure
-#3. check dimensions of data with dim() 
-#4. plot my_aggr() to find patterns in missing data
-#5. remove variables that have many observations are missing, if necessary
-#6. remove all observations that still have missing variables
-
-##### Dsps_Food
-Dsps_Food.data <- data %>%
-  select(-Electronics,-Percrip_Med,-Drn_Clnr) %>%
-  filter(!is.na(Dsps_Food)) 
-
-dim(Dsps_Food.data)# 2858 rows, 11 columns after other measures removed
+#for each measure of customer environmental behavior:
+#1. filter for observations where measure is not missing
+#2. remove Inc_Cat; >10% of ovservations are missing only that variable
+#3. remove all remaining observations that are missing variables
 my_aggr(Dsps_Food.data)
 
-Dsps_Food.data <- Dsps_Food.data %>%
-  select(-Inc_Cat) %>% #10.9% of observations missing only Inc_Cat
-  na.omit
-
-dim(Dsps_Food.data)# 2588 observations, 10 variables in pure dataset
-
-##### Electronics
-Electronics.data <- data %>%
-  select(-Dsps_Food,-Percrip_Med,-Drn_Clnr) %>%
-  filter(!is.na(Electronics)) 
-
-dim(Electronics.data)# 2700 rows, 11 columns after other measures removed
-my_aggr(Electronics.data)
-
-Electronics.data <- Electronics.data %>%
-  select(-Inc_Cat) %>% #10.9% of observations missing only Inc_Cat
-  na.omit
-
-dim(Electronics.data)# 2415 observations, 10 variables in pure dataset
-
-##### Percrip_Med
-Percrip_Med.data <- data %>%
-  select(-Electronics,-Dsps_Food,-Drn_Clnr) %>%
-  filter(!is.na(Percrip_Med)) 
-
-dim(Percrip_Med.data)# 2111 rows, 11 columns after other measures removed
-my_aggr(Percrip_Med.data)
-
-Percrip_Med.data <- Percrip_Med.data %>%
-  select(-Inc_Cat) %>% #10.9% of observations missing only Inc_Cat
-  na.omit
-
-dim(Percrip_Med.data)# 1883 observations, 10 variables in pure dataset
-
-##### Drn_Clnr
-Drn_Clnr.data <- data %>%
-  select(-Electronics,-Percrip_Med,-Dsps_Food) %>%
-  filter(!is.na(Drn_Clnr)) 
-
-dim(Drn_Clnr.data)# 2367 rows, 11 columns after other measures removed
-my_aggr(Drn_Clnr.data)
-
-Drn_Clnr.data <- Drn_Clnr.data %>%
-  select(-Inc_Cat) %>% #10.9% of observations missing only Inc_Cat
-  na.omit
-
-dim(Drn_Clnr.data)# 2151 observations, 10 variables in pure dataset
-
+##### Dsps_Food
+data.list <- list("Electronics"=NULL,"Percrip_Med"=NULL,"Drn_Clnr"=NULL,"Dsps_Food"=NULL)
+i <- 1
+for(var in names(data)[11:14]){
+  data.list[[i]] <- data %>%
+    filter_(!is.na(var)) %>%
+    select(-Inc_Cat) %>% #10.9% of observations missing only Inc_Cat
+    na.omit
+  i <- i+1
+}
