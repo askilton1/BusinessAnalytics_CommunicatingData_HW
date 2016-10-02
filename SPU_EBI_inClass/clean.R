fe8aca482219059fc6a4cccfc0dc846ea04d39d9
askilton1 <antonio.skilton@gmail.com>
2016-10-02 08:07:17 -0700

    clean.r is now clean()

--- SPU_EBI_inClass/clean.R
+++ SPU_EBI_inClass/clean.R
@@ -1,14 +1,23 @@
-library(dplyr)
-library(tidyr)
+clean <- function(df,plot=FALSE){
+suppressMessages(library(dplyr))
+suppressMessages(library(tidyr))
+suppressMessages(library(psych))
 source("my_aggr.R")
 
-data2 <- tbl_df(read.csv("2_EBI_Data.csv"))
 ##MISSING VARIABLES
-data3<- data2 %>%
+if(plot==TRUE) my_aggr(raw)#plot shows patterns of missing data in observations
+df<- raw %>%
   select(-Inc_Cat) %>%
   gather(key=response,value=rating,Electronics,Percrip_Med,Drn_Clnr,Dsps_Food) 
-
+if(plot==TRUE) my_aggr(df)
 
 ##COLINEARITY
-library(psych)
-pairs.panels(data3)
+if(plot==TRUE) pairs.panels(raw)#plot shows correlation between variables
+
+df <- df %>%
+  select(-Age_Cat)#colinear with Age variable
+
+if(plot==TRUE) pairs.panels(select(df,-response))#in second plot, response var has 0 correlation
+
+return(df)
+}
