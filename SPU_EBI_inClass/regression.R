ccae9c4908c0a1e6339cad6c66cab81a1f8d6951
askilton1 <antonio.skilton@gmail.com>
2016-10-02 09:41:26 -0700

    move glmnet() to lasso_fun

--- SPU_EBI_inClass/regression.R
+++ SPU_EBI_inClass/regression.R
@@ -8,11 +8,10 @@ stargazer(lm(rating~.,data=data.frame(EBI$electronicsLASSO)),
           lm(rating~.,data=data.frame(model.matrix(~.,EBI$df))),
           type="text",no.space=TRUE)
 
-library(glmnet)
 ##LASSO variable selection
 lasso.fun(EBI$electronicsLASSO)$coefficients
 lasso.fun(EBI$electronicsLASSO)$RMSE
-
+#     results[8,1] <- ipred::errorest(mnyordEVER~.,data=test,model=qda,estimator="cv",predict=mypredict.pca)$error
 
 electronics.mod <- glmnet(EBI$electronicsLASSO[,-24],EBI$electronicsLASSO[,24],alpha=1,lambda=EBI$grid)
     set.seed(1)
