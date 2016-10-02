9175efd239c19f4f288643de1415c1fcf6df36d1
askilton1 <antonio.skilton@gmail.com>
2016-10-02 08:07:53 -0700

    linear regression models up

--- SPU_EBI_inClass/regression.R
+++ SPU_EBI_inClass/regression.R
@@ -1,1 +1,9 @@
-summary(lm(rating~.,data=select(filter(data3,response=="Electronics"),-response)))
+source("clean.R")
+df <- clean(read.csv("2_EBI_Data.csv"))
+library(stargazer)
+stargazer(lm(rating~.,data=select(filter(df,response=="Electronics"),-response)),
+          lm(rating~.,data=select(filter(df,response=="Percrip_Med"),-response)),
+          lm(rating~.,data=select(filter(df,response=="Drn_Clnr"),-response)),
+          lm(rating~.,data=select(filter(df,response=="Dsps_Food"),-response)),
+          type="text",no.space=TRUE)
+?stargazer
