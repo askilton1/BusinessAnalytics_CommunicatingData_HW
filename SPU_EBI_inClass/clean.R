e057ebc36803a72454210c4a074040b27d6f9297
askilton1 <antonio.skilton@gmail.com>
2016-10-01 19:22:19 -0700

    gather()
    much more simple to use tidyr’s gather() function to reshape data. I
    then subset within lm functions using dplyr

--- SPU_EBI_inClass/clean.R
+++ SPU_EBI_inClass/clean.R
@@ -1,21 +1,14 @@
 library(dplyr)
+library(tidyr)
 source("my_aggr.R")
 
-data <- tbl_df(read.csv("2_EBI_Data.csv"))
+data2 <- tbl_df(read.csv("2_EBI_Data.csv"))
+##MISSING VARIABLES
+data3<- data2 %>%
+  select(-Inc_Cat) %>%
+  gather(key=response,value=rating,Electronics,Percrip_Med,Drn_Clnr,Dsps_Food) 
 
-#for each measure of customer environmental behavior:
-#1. filter for observations where measure is not missing
-#2. remove Inc_Cat; >10% of ovservations are missing only that variable
-#3. remove all remaining observations that are missing variables
-my_aggr(Dsps_Food.data)
 
-##### Dsps_Food
-data.list <- list("Electronics"=NULL,"Percrip_Med"=NULL,"Drn_Clnr"=NULL,"Dsps_Food"=NULL)
-i <- 1
-for(var in names(data)[11:14]){
-  data.list[[i]] <- data %>%
-    filter_(!is.na(var)) %>%
-    select(-Inc_Cat) %>% #10.9% of observations missing only Inc_Cat
-    na.omit
-  i <- i+1
-}
+##COLINEARITY
+library(psych)
+pairs.panels(data3)
