07035d20731f56b862f9e359c16e95e5e9003d2f
askilton1 <antonio.skilton@gmail.com>
2016-10-06 00:18:08 -0700

    ordered Education factor, added na.omit for $completeData

--- SPU_EBI_inClass/clean.R
+++ SPU_EBI_inClass/clean.R
@@ -1,22 +1,34 @@
-clean <- function(raw,plot=FALSE,LASSO=TRUE,ALL=TRUE){
+clean <- function(raw_data,plot=FALSE,LASSO=TRUE,ALL=TRUE){
   suppressMessages(library(dplyr))
   suppressMessages(library(tidyr))
   suppressMessages(library(psych))
   source("my_aggr.R")
   
+  raw_data <- tbl_df(raw_data)
+  
   ##MISSING VARIABLES
-  if(plot==TRUE) my_aggr(raw)#plot shows patterns of missing data in observations
-  df<- raw %>%
-    select(-Inc_Cat) %>%
-    gather(key=response,value=rating,Electronics,Percrip_Med,Drn_Clnr,Dsps_Food) 
+  if(plot==TRUE) my_aggr(raw_data)#plot shows patterns of missing data in observations
+  
+  df <- raw_data %>%
+    select(-Inc_Cat) %>% #missing too many observations!
+    gather(key=response,value=rating,Electronics,Percrip_Med,Drn_Clnr,Dsps_Food) %>%
+    mutate(Education = factor(Education, levels = c("Non-high school graduate","High school diploma / GED", 
+                                                    "Some college", "Associates Degree", "Bachelor's Degree", 
+                                                    "Some graduate work", "Graduate degree(s)", 
+                                                    "Missing / Prefer not to answer")))
   if(plot==TRUE) my_aggr(df)
   
   ##COLINEARITY
-  if(plot==TRUE) pairs.panels(raw)#plot shows correlation between variables
-  data.ls <- list()
-  data.ls$completeData <- df %>%
-    select(-Age_Cat)#colinear with Age variable
+  if(plot==TRUE) pairs.panels(raw_data)#plot shows correlation between variables
   
+  data.ls <- list()
+  
+  df <- df %>%
+    select(-Age_Cat) #colinear with Age variable
+    
+  
+  data.ls$completeData <- na.omit(df)
+    
   if(plot==TRUE) pairs.panels(select(df,-response))#in second plot, response var has 0 correlation
   
   if(LASSO==TRUE){
